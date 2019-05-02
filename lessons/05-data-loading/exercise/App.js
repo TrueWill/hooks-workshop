b4e734c1a429f2ec67469260850311c40acda7c8
Bill Sorensen <baldgoth@gmail.com>
2019-05-02 15:39:56 -0500

    Lesson 5

--- lessons/05-data-loading/exercise/App.js
+++ lessons/05-data-loading/exercise/App.js
@@ -1,20 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 
-import { onAuthStateChanged } from 'app/utils'
-import LoggedIn from 'app/LoggedIn'
-import LoggedOut from 'app/LoggedOut'
+import { onAuthStateChanged } from 'app/utils';
+import LoggedIn from 'app/LoggedIn';
+import LoggedOut from 'app/LoggedOut';
+
+const useAuth = () => {
+  const [auth, setAuth] = useState(null);
+  const [authAttempted, setAuthAttempted] = useState(false);
+
+  useEffect(() => {
+    const cleanup = onAuthStateChanged(a => {
+      setAuthAttempted(true);
+      setAuth(a);
+    });
+
+    return cleanup;
+  }, []);
+
+  return { auth, authAttempted };
+};
 
 export default function App() {
-  const auth = null
-  const authAttempted = false
+  /* const [auth, setAuth] = useState(null);
+  const [authAttempted, setAuthAttempted] = useState(false);
+
+  useEffect(() => {
+    const cleanup = onAuthStateChanged(a => {
+      setAuthAttempted(true);
+      setAuth(a);
+    });
+
+    return cleanup;
+  }, []); */
+
+  const { auth, authAttempted } = useAuth();
+  // The warning on logout is due to a bug in onAuthStateChanged.
 
   if (!authAttempted) {
-    return <p>Authenticating...</p>
+    return <p>Authenticating...</p>;
   }
 
   return (
     <div className="Layout">
       {auth ? <LoggedIn auth={auth} /> : <LoggedOut />}
     </div>
-  )
+  );
 }
