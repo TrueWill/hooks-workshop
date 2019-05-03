e3831cce0380dc563194f8f84598a3c41e6408e8
Bill Sorensen <baldgoth@gmail.com>
2019-05-03 13:44:24 -0500

    Lesson 10 step 1

--- lessons/10-the-feed/exercise/Feed.js
+++ lessons/10-the-feed/exercise/Feed.js
@@ -1,11 +1,35 @@
-import React from "react"
-import FeedPost from "app/FeedPost"
-import { loadFeedPosts, subscribeToNewFeedPosts } from "app/utils"
+import React, { useState, useEffect } from 'react';
+import FeedPost from 'app/FeedPost';
+import { loadFeedPosts, subscribeToNewFeedPosts } from 'app/utils';
 // import FeedFinal from './Feed.final'
 // export default FeedFinal
-export default Feed
+export default Feed;
 
 function Feed() {
+  const [limit, setLimit] = useState(3);
+  const [time, setTime] = useState(Date.now());
+  const [posts, setPosts] = useState(null);
+
+  useEffect(() => {
+    let isCurrent = true;
+
+    loadFeedPosts(time, limit).then(posts => {
+      if (isCurrent) {
+        setPosts(posts);
+      }
+    });
+
+    return () => {
+      isCurrent = false;
+    };
+  }, [time, limit]);
+
+  if (!posts) {
+    return null;
+  }
+
+  const feedPosts = posts.map(post => <FeedPost post={post} key={post.id} />);
+
   return (
     <div className="Feed">
       <div className="Feed_button_wrapper">
@@ -14,21 +38,11 @@ function Feed() {
         </button>
       </div>
 
-      <FeedPost post={fakePost} />
+      {feedPosts}
 
       <div className="Feed_button_wrapper">
         <button className="Feed_new_posts_button icon_button">View More</button>
       </div>
     </div>
-  )
+  );
 }
-
-// you can delete this
-const fakePost = {
-  createdAt: Date.now() - 10000,
-  date: "2019-03-30",
-  message: "Went for a run",
-  minutes: 45,
-  uid: "0BrC0fB6r2Rb5MNxyQxu5EnYacf2"
-}
-
